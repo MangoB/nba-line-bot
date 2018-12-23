a46eb38ff6a97dbee840e0c5fbe24afb425fec92
Sinlapakorn Aunjitt <sinlapakorn_aun@true-e-logistics.com>
2018-12-24 00:36:58 +0700

    ah

--- API/Routes/Routes.js
+++ API/Routes/Routes.js
@@ -1,11 +1,30 @@
 "use strict";
-var express = require("express");
+// var express = require("express");
 module.exports = function(app) {
-  var gameDataController = require("../Controllers/GameDataController");
-  var apiRoutes = express.Router();
+  // var gameDataController = require("../Controllers/GameDataController");
+  // var apiRoutes = express.Router();
   app.get("/", function(req, res) {
     res.send("We are happy to see you using Chat Bot Webhook");
   });
   // registerUser Route
-  app.route("/").post(gameDataController.processRequest);
+  // app.route("/").post(gameDataController.processRequest);
+  app.route("/").post(
+    res.json({
+      speech:
+        "Last game between Kings and " +
+        parameters.team +
+        " was played on " +
+        "25/11/2561" +
+        " ." +
+        "Kings won this match by 120 - 119",
+      displayText:
+        "Last game between Kings and " +
+        parameters.team +
+        " was played on " +
+        "25/11/2561" +
+        " ." +
+        "Kings won this match by 120 - 119",
+      source: "game schedule"
+    })
+  );
 };
