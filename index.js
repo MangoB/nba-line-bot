48b1dceec4c5477d90badfa641890149436965c0
Sinlapakorn Aunjitt <sinlapakorn_aun@true-e-logistics.com>
2018-12-24 00:29:51 +0700

    edit file name

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var express = require("express"),
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 // mongoose.connect(config.dbUrl);
+
 mongoose
   .connect(
     "mongodb://ds233739.mlab.com:33739/nba",
