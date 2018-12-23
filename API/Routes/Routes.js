1d04df92928ae670c430b038838cb1bbb71e18da
Sinlapakorn Aunjitt <sinlapakorn_aun@true-e-logistics.com>
2018-12-24 00:48:12 +0700

    3

--- API/Routes/Routes.js
+++ API/Routes/Routes.js
@@ -1,30 +1,30 @@
 "use strict";
 // var express = require("express");
 module.exports = function(app) {
-  // var gameDataController = require("../Controllers/GameDataController");
+  var gameDataController = require("../Controllers/GameDataController");
   // var apiRoutes = express.Router();
   app.get("/", function(req, res) {
     res.send("We are happy to see you using Chat Bot Webhook");
   });
   // registerUser Route
-  // app.route("/").post(gameDataController.processRequest);
-  app.post("/", function(req, res) {
-    res.send({
-      speech:
-        "Last game between Kings and " +
-        parameters.team +
-        " was played on " +
-        "25/11/2561" +
-        " ." +
-        "Kings won this match by 120 - 119",
-      displayText:
-        "Last game between Kings and " +
-        parameters.team +
-        " was played on " +
-        "25/11/2561" +
-        " ." +
-        "Kings won this match by 120 - 119",
-      source: "game schedule"
-    })
-  })
+  app.route("/").post(gameDataController.processRequest);
+  // app.post("/", function(req, res) {
+  //   res.send({
+  //     speech:
+  //       "Last game between Kings and " +
+  //       parameters.team +
+  //       " was played on " +
+  //       "25/11/2561" +
+  //       " ." +
+  //       "Kings won this match by 120 - 119",
+  //     displayText:
+  //       "Last game between Kings and " +
+  //       parameters.team +
+  //       " was played on " +
+  //       "25/11/2561" +
+  //       " ." +
+  //       "Kings won this match by 120 - 119",
+  //     source: "game schedule"
+  //   })
+  // })
 };
